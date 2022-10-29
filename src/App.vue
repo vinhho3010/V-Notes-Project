44d5f940f949ce18ac1366ece8aed0250db8b03b
vinhho3010 <anhvinh30102001@gmail.com>
2022-10-29 17:26:10 +0700

    inittial frontend

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container m-0">
+      <div class="w-full m-0 p-0">
+        <Header/>
+      </div>
+
+      <div class="w-full ">
+        <div class="mx-auto"><router-view/></div>
+        
+      </div>
+    </div>
+    
+
+</template>
+
+<script>
+  import Header from '@/components/Header.vue';
+  
+    export default {
+      components: {
+        Header,
+      },
+    };
+</script>
+
+
+<style>
+ 
+
+  
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class=" flex items-center justify-items-center justify-between flex-wrap bg-[#579981] p-4">
+    <div class="flex items-center flex-shrink-0 ml-[45%] text-white mr-6">
+      <img
+        class="fill-current h-8 w-8 mr-2"
+        width="74"
+        height="74"
+        viewBox="0 0 54 54"
+        src="../assets/MainIcon.png"
+      />
+      <span class="font-semibold text-xl tracking-tight">V-Notes</span>
+    </div>
+      <div>
+        <a
+          href="#"
+          class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+          >Đăng ký</a
+        >
+      </div>
+  </nav>
+</template>
+
